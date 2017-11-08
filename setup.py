f889a629ee359eb460d5ecb932c035051ad8394a
Gustavo Baratto <gbaratto@demonware.net>
2017-11-08 01:01:05 +0000

    Add licensing

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+# Copyright 2017 Gustavo Baratto. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 from setuptools import find_packages
 from setuptools import setup
@@ -32,8 +46,8 @@ config = {
         "console_scripts": ["gpw=gpw.cli:main"]
     },
     "setup_requires": ["pytest-runner"],
-    "install_requires": install_requires,
-    "tests_require": tests_require
+    "install_requires": get_install_requirements(),
+    "tests_require": get_test_requirements()
 }
 
 
